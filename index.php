84a2248961beefdefddaee0e09ef4871a593f5c7
Al Nemec <al.nemec@gmail.com>
2014-02-26 21:04:03 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+Plugin Name: Modern Admin UI
+Plugin URI: http://alnemec.com/modern-admin-ui/
+Description: Modern and clean WordPress Admin Theme.
+Author: Al Nemec
+Version: 1.0
+Author URI: http://alnemec.com
+*/
+
+function my_admin_theme_style() {
+    wp_enqueue_style('my-admin-theme', plugins_url('wp-clean-admin.css', __FILE__));
+    wp_enqueue_script(
+		'my-admin-theme-actions',
+		plugins_url( '/wp-clean-admin.js' , __FILE__ ),
+		array( 'jquery' )
+	);
+}
+add_action('admin_enqueue_scripts', 'my_admin_theme_style');
+add_action('login_enqueue_scripts', 'my_admin_theme_style');
+
+
+function my_login_css() {
+  echo '<link rel="stylesheet" type="text/css" href="' .plugins_url('login.css  ', __FILE__). '">';
+}
+
+add_action('login_head', 'my_login_css');
+
+
+
+?>
---
+++ wp-clean-admin.js
@@ -0,0 +1,400 @@
+jQuery(document).ready(function($) {
+
+
+ function setGetParameter(url, paramName, paramValue)
+{
+	
+    //var url = window.location.href;
+    if (url.indexOf(paramName + "=") >= 0)
+    {
+    	console.log(paramName);
+        var prefix = url.substring(0, url.indexOf(paramName));
+        var suffix = url.substring(url.indexOf(paramName)).substring(url.indexOf("=") + 1);
+        suffix = (suffix.indexOf("&") >= 0) ? suffix.substring(suffix.indexOf("&")) : "";
+        url = prefix + paramName + "=" + paramValue + suffix;
+    }
+    else
+    {
+if (url.indexOf("?") < 0) 
+        url += "?" + paramName + "=" + paramValue;
+    else
+        url += "&" + paramName + "=" + paramValue;
+    }
+    return url;
+}
+
+function gup( name ){
+name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");  
+var regexS = "[\\?&]"+name+"=([^&#]*)";  
+var regex = new RegExp( regexS );  
+var results = regex.exec( window.location.href ); 
+ if( results == null )    return "";  
+else    return results[1];}
+
+
+ var siteurl = $("#wp-admin-bar-site-name a").attr("href") + "wp-admin/";
+var currentURL = document.URL;
+
+/*$("#adminmenuwrap").fadeIn(300);*/
+
+$('#adminmenuwrap').animate({
+    opacity: 1
+   
+  }, 400, function() {
+    
+  });
+
+$('#wpbody').animate({
+    opacity: 1   
+	  }, 300, function() {
+	    // Animation complete.
+	  });
+
+	if(currentURL == siteurl) {
+	
+	//sessionStorage.removeItem("section");
+	 sessionStorage.setItem("section", "0");
+	 // Handler for .ready() called.
+	$("body").append('<div id="whiteout"><div id="wplogogray"></div></div>');
+	setTimeout(function() {
+	$('#whiteout #wplogogray').addClass("animate");
+	
+	},300);
+	setTimeout(function() {
+		
+		$('#whiteout').fadeOut(400, function() {
+	    // Animation complete.
+		});
+		
+	},1600);
+	
+	}
+
+ 
+
+
+var analytics = false;
+var analyticsUrl;
+
+//Check to see if JetPack analytics is installed
+$( "a" ).each(function( index ) {
+	 if($(this).text() == "Site Stats") {
+		analyticsUrl = $(this).attr("href");
+		analytics = true;
+		
+	} 
+ });
+ 
+var sectioncheck = gup("section");
+
+if (sectioncheck) {
+$( "#wpbody a" ).each(function( index ) {
+	 var h = $(this).attr("href");
+	 	if(h) {
+	 	h = setGetParameter(h, "section", sectioncheck);
+	 	 $(this).attr("href",h);
+	 	}
+	
+ });
+ 
+ $( "#wpbody input" ).each(function( index ) {
+	 var i = $(this).attr("type");
+	 if(i == "submit") {
+		 var s = $(this).closest("form").attr("action");
+		 //console.log(s);
+		 if(s) {
+			 //s = setGetParameter(s, "section", sectioncheck);
+			 //$(this).closest("form").attr("action",s);
+			 
+		 }
+		 
+		 
+	 }
+	
+ });
+ 
+ sessionStorage.setItem("section", sectioncheck);
+}
+ 
+
+ 
+ if(analytics) {
+ $("#wpwrap").prepend('<div class="newGlobalMenu"><nav class="cleanNav"><a href="#" class="dashboardButton active">Dashboard<span></span></a><a href="#" class="analyticsButton" data-section="0">Statistics<span></span></a><a href="#" class="contentButton">Content<span></span></a><a href="#" class="settingsButton">Settings<span></span></a></nav><div class="wplogo"></div><a href="#" class="previewButton"><span class="previewButtonText"></span><div class="previewFade"></div></a></div>'); 
+ } else {
+	 $("#wpwrap").prepend('<div class="newGlobalMenu"><nav class="cleanNav"><a href="#" class="dashboardButton active">Dashboard<span></span></a><a href="#" class="contentButton">Content<span></span></a><a href="#" class="settingsButton">Settings<span></span></a></nav><div class="wplogo"></div><a href="#" class="previewButton"><span class="previewButtonText"></span><div class="previewFade"></div></a></div>');
+ }
+
+$("#wpcontent").prepend('<div id="splash"></div>');
+ 
+ 
+ $("#adminmenu").prepend('<li class="wp-not-current-submenu wp-menu-separator"><h3 class="dashboard">Dashboard</h3><p>Get a view of everything in wordpress from the dashboard.</p></li>');
+ 
+ var siteurl = $("#wp-admin-bar-site-name a").attr("href");
+ var sitename = $("#wp-admin-bar-site-name a").first().text();
+ 
+ if(analyticsUrl) {
+ 	analyticsUrl = setGetParameter(analyticsUrl, "section", "0");
+ }
+ 
+ var updatesurl = $("#wp-admin-bar-updates a").attr("href");
+ if(updatesurl) {
+ 	updatesurl = setGetParameter(updatesurl, "section", "0");
+ }
+ 
+ var dashboardurl = $("a.menu-icon-dashboard").attr("href");
+ if(dashboardurl) {
+ 	dashboardurl = setGetParameter(dashboardurl, "section", "0");
+ }
+
+ 
+ var contenturl = siteurl + "wp-admin/index.php?section=1&splash=content";
+ 
+ var splashresult = gup("splash");
+ 
+if(splashresult) {
+	$("#wpcontent #splash").show();
+	$( "#wpcontent #splash" ).addClass(splashresult);  	
+} else {
+	$("#wpcontent #splash").hide();
+}
+ 
+ //var settingsurl = $("a.menu-icon-appearance").attr("href");
+ //settingsurl = setGetParameter(settingsurl, "section", "2");
+ 
+ $(".previewButton").attr("href",siteurl);
+ $(".previewButtonText").text(sitename);
+ $(".cleanNav .dashboardButton").attr("href",dashboardurl);
+ $(".cleanNav .contentButton").attr("href",contenturl);
+ $(".cleanNav .analyticsButton").attr("href",analyticsUrl);
+ $("#wp-admin-bar-updates a").attr("href",updatesurl);
+ 
+ 
+
+
+
+ var foundposts = false;
+ var foundmultisite = false;
+ 
+ $( "li.wp-menu-separator" ).each(function( index ) {
+ 		$(this).attr("data-section",index);
+ 		$(this).nextAll("li").attr("data-section",index);
+ 		
+ 		
+ 		
+ 		
+ 		var t = $(this).next().find(".wp-menu-name").text();
+ 		
+	  	if(t == "Posts") {
+		  	$(this).html("<h3 class='content'>Content</h3><p>Create and edit your content.</p>");
+		  	foundposts = true;
+	  	}
+	  	
+	  	if(t == "Sites") {
+	  		$(this).html("<h3 class='settings'>Configure</h3><p>Change settings for your site, access import and export tools, and adjust user information.</p>");
+		  	foundmultisite = true;
+		  	 var settingsurl = $("a.menu-icon-dashboard").attr("href");
+			 if(settingsurl) {
+			 	settingsurl = setGetParameter(settingsurl, "section", "1");
+			 	settingsurl = setGetParameter(settingsurl, "splash", "settings");
+			 }
+		  	$(".cleanNav .settingsButton").attr("href",settingsurl);
+	  	}
+	  	
+	  	if(t == "Appearance" && foundmultisite == false) {
+		  	$(this).html("<h3 class='settings'>Configure</h3><p>Change settings for your site, access import and export tools, and adjust user information.</p>");
+		  	
+		  	  var settingsurl = $("a.menu-icon-dashboard").attr("href");
+			 if(settingsurl) {
+			 	settingsurl = setGetParameter(settingsurl, "section", "2");
+			 	settingsurl = setGetParameter(settingsurl, "splash", "settings");
+			 }
+			 $(".cleanNav .settingsButton").attr("href",settingsurl);
+		  	
+	  	} 
+  	});
+  	
+  if(!foundposts) {
+	  $(".contentButton").hide();
+  }
+  
+  	
+  	
+  	$( "#adminmenu a" ).each(function( index ) {
+ 		var urlstart = $(this).attr("href");
+ 		var setindex = $(this).closest("li.menu-top").attr("data-section");
+ 		
+ 		if(setindex) {
+ 		var parm = setGetParameter(urlstart,"section",setindex);
+ 		$(this).attr("href",parm);
+ 		//console.log(setindex);
+ 		}
+ 		
+  	});
+  	
+  	
+  	$( "#wpadminbar a" ).each(function( index ) {
+ 		var urlstart = $(this).attr("href");
+ 		
+ 		if($(this).closest("li").attr("id") == "wp-admin-bar-network-admin-s") {
+	 		var parm = setGetParameter(urlstart,"section","1");
+ 		} else if($(this).closest("li").attr("id") == "wp-admin-bar-network-admin-u") {
+	 		var parm = setGetParameter(urlstart,"section","1");
+ 		} else if($(this).closest("li").attr("id") == "wp-admin-bar-blog-1-n") {
+	 		var parm = setGetParameter(urlstart,"section","1");
+ 		} else if($(this).closest("li").attr("id") == "wp-admin-bar-blog-1-c") {
+	 		var parm = setGetParameter(urlstart,"section","1");
+ 		} else {
+	 		var parm = setGetParameter(urlstart,"section","0");
+ 			
+ 		}
+ 		$(this).attr("href",parm);
+ 		
+ 		
+  	});
+  	
+  	 var siteurli = $("#wp-admin-bar-site-name a").attr("href") + "wp-admin/";
+var currentURL = document.URL;
+  	
+ if(currentURL == siteurli) {
+	
+		$( "#adminmenu li.menu-top, #adminmenu li.wp-menu-separator" ).each(function( index ) {
+	   		var t = $(this).attr("data-section");
+	   		
+		  	if(t == "0") {
+			  	$(this).show();
+			  	
+		  	} else {
+			  	$(this).hide();
+			  	
+		  	}
+		 });
+	
+	
+	
+	
+	}
+  var sessionstorage = sessionStorage.getItem("section");
+ 
+
+var sectionresult = gup("section");
+
+
+if(sessionstorage) {
+	 $( "#adminmenu li.menu-top, #adminmenu li.wp-menu-separator" ).each(function( index ) {
+	   		var t = $(this).attr("data-section");
+	   		
+		  	if(sessionstorage == t) {
+			  	$(this).show();
+			  	
+		  	} else {
+			  	$(this).hide();
+			  	
+		  	}
+		 });
+		 console.log("used session storage");
+ } else {
+
+if(sectionresult) {
+	
+		$( "#adminmenu li.menu-top, #adminmenu li.wp-menu-separator" ).each(function( index ) {
+	   		var t = $(this).attr("data-section");
+	   		
+		  	if(sectionresult == t) {
+			  	$(this).show();
+			  	
+		  	} else {
+			  	$(this).hide();
+			  	
+		  	}
+		 });
+		 console.log("used url parm");
+  	
+} 
+}
+  	
+  $("#adminmenu a").click(function() {
+  	//return false;
+ 	var l = $(this).closest("li.menu-top").attr("data-section");
+ 	//var url = $(this).attr("href") + "&foo=bar";
+ 	//$(this).attr("href",url);
+ 	
+ 	$( "#adminmenu li.menu-top" ).each(function( index ) {
+   		var t = $(this).attr("data-section");
+   		
+	  	if(l == t) {
+		  	$(this).show();
+		  	
+	  	} else {
+		  	$(this).hide();
+		  	
+	  	}
+  	});
+  	
+  	
+ });
+ 
+ 
+
+
+ 
+ 
+/*$( "#adminmenu li.menu-icon-dashboard" ).attr("data-section","9");*/
+
+
+ /*$(".cleanNav .dashboardButton").click(function() {
+ 	
+   $( "#adminmenu li.menu-top" ).each(function( index ) {
+	  	if($(this).hasClass("menu-icon-dashboard")) {
+		  	$(this).show();
+		  	//$(this).attr("data-show","yes");
+	  	} else {
+		  	$(this).hide();
+		  	//$(this).attr("data-show","no");
+	  	}
+  	});
+ });
+ 
+ $(".cleanNav .dashboardButton").click(function() {
+   $( "#adminmenu li.menu-top" ).each(function( index ) {
+	  	
+		  	$(this).show();
+		  	//$(this).attr("data-show","yes");
+  	});
+ });*/
+ 
+
+ /* $(".cleanNav .contentButton").mouseover(function() {
+   $( "#adminmenu li.menu-top" ).each(function( index ) {
+	  	if($(this).hasClass("menu-icon-post") || $(this).hasClass("menu-icon-media") || $(this).hasClass("menu-icon-page") || $(this).hasClass("menu-icon-comments")) {
+		  	$(this).show();
+	  	} else {
+		  	$(this).hide();
+	  	}
+  	});
+ });
+ 
+ $(".cleanNav .contentButton").mouseout(function() {
+   $( "#adminmenu li.menu-top" ).each(function( index ) {
+	  	
+		  	$(this).show();
+	  	
+  	});
+ });*/
+ 
+ 
+
+ 
+  WebFontConfig = {
+    google: { families: [ 'Open+Sans:300,600:latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })(); 
+ 
+});
